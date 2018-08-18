25089d03fac86d0ea9e9abdfe0f33ed14ce0ff2b
David Kang <dkang009@gmail.com>
2018-08-18 12:03:58 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load();
+
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+ 
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
+
