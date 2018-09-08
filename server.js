a36ffc0732322f6bd2935517c39ed31eb18f03b3
David Kang <dkang009@gmail.com>
2018-09-08 12:02:10 -0500

    cosmetic changes

--- server.js
+++ server.js
@@ -13,19 +13,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
- 
 app.use(cors());
 
- 
 // For Passport
 app.use(session({
     secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true
 })); // session secret
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
- 
  
 //For Handlebars
 app.set('views', './app/views')
@@ -33,8 +31,6 @@ app.engine('hbs', exphbs({
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
- 
- 
  
 app.get('/', function(req, res) {
     res.send('Welcome to Passport with Sequelize');
@@ -53,20 +49,10 @@ require('./app/config/passport/passport.js')(passport, models.user);
  
 //Sync Database
 models.sequelize.sync().then(function() {
- 
     console.log('Nice! Database looks fine')
- 
- 
 }).catch(function(err) {
- 
     console.log(err, "Something went wrong with the Database Update!")
- 
 });
- 
- app.post("/testd", (req, res) => {
-     console.log('hi');
-     res.send({hi: 'hello'})
- })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function(err) {
